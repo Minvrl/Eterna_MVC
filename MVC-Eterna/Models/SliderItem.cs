0e552b88c8a6dc2470e589e08c41b197696118c4
Minvrl <m.novruzluu@gmail.com>
2024-04-21 23:16:23 +0400

    first

---
+++ MVC-Eterna/Models/SliderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Eterna.Models
+{
+    public class SliderItem
+    {
+        public int Id { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Image { get; set; }
+        public int Order { get; set; }
+    }
+}
